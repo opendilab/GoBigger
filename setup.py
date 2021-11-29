10e9cfa0a7b5161f42b0e1c001454464b47cfec7
Ming Zhang <18401693787@163.com>
2021-11-29 17:28:16 +0800

    Update setup.py
    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='gobigger',
-    version='0.1.2',
+    version='0.1.3',
     description='Go-Bigger: Multi-Agent Decision Intelligence Environment',
     author='OpenDILab',
     license='Apache License, Version 2.0',
