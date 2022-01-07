e04fe0d5211a96ee67cf3b4f97b7ab41c6df78b7
mingzhang96 <zmhappyer@gmail.com>
2022-01-07 13:53:04 +0800

    add stress

--- gobigger/bin/stress_test.py
+++ gobigger/bin/stress_test.py
@@ -24,21 +24,24 @@ def launch(replay_dir='replays'):
         bot_agents.append(BotAgent(player_name, level=3))
     logging.info(seed)
     data_simple = {'seed': seed, 'actions': []}
+    replay_path = os.path.join(replay_dir, str(seed)+'.replay')
+    f = open(replay_path, 'w')
+    f.write(str(seed) + '\n')
     for i in range(100000):
         obs = server.obs()
         actions = {bot_agent.name: bot_agent.step(obs[1][bot_agent.name]) for bot_agent in bot_agents}
         data_simple['actions'].append(actions)
         # logging.info('{} {} {} {} {}'.format(seed, i, server.get_last_time(), actions, obs[0]['leaderboard']))
-        try:
-            finish_flag = server.step(actions=actions)
-            if finish_flag:
-                logging.info('Game Over, {}'.format(obs[0]['leaderboard']))
-                break
-        except Exception as e:
-            replay_path = os.path.join(replay_dir, str(seed)+'.replay')
-            with open(replay_path, "wb") as f:
-                pickle.dump(data_simple, f)
+        f.write('{} {}\n'.format(i, actions))
+        f.flush()
+        finish_flag = server.step(actions=actions)
+        if finish_flag:
+            logging.info('Game Over, {}'.format(obs[0]['leaderboard']))
+            break
     server.close()
+
+
+# def relaunch(replay_path):
 
 
 if __name__ == '__main__':
@@ -47,3 +50,5 @@ if __name__ == '__main__':
         os.mkdir(replay_dir)
     while True:
         launch(replay_dir)
+
+
