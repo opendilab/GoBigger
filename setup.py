184f6432abb772c17dab01aedd35de9d85f6c634
zhangming <zhangming@sensetime.com>
2022-05-26 23:43:10 +0800

    0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='gobigger',
-    version='0.1.5',
+    version='0.2.0',
     description='Go-Bigger: Multi-Agent Decision Intelligence Environment',
     author='OpenDILab',
     license='Apache License, Version 2.0',
