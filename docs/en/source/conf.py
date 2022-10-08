e7e9748bf64a2cfef65a7e3f441d6f5dacab561f
zhangming <zhangming@sensetime.com>
2022-10-08 17:59:50 +0800

    update doc and config

--- docs/en/source/conf.py
+++ docs/en/source/conf.py
@@ -10,7 +10,7 @@ author = 'OpenDILab Contributors'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 
